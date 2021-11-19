6a48a42516b9299b6704428003f1759b5102bb1b
Siddharth Bisht <siddharthbisht99@gmail.com>
2021-11-19 12:26:27 +0530

    Solved DCP 42

--- DCP10000.py
+++ DCP10000.py
@@ -1,30 +1,52 @@
-# Definition for a binary tree node.
+from typing import List
 
-from typing import List, Optional
-
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
 
 class Solution:
-    def helper(self, nums: List[int],leftIndex:int,rightIndex:int):
-        if rightIndex  <= leftIndex:
-            return TreeNode(None)
-        print(leftIndex,rightIndex)
-        
-        maximumIndex = leftIndex
-        for x in range(rightIndex-leftIndex+1):
-            if nums[leftIndex + x]>nums[maximumIndex]:
-                maximumIndex = leftIndex + x
-        print(nums[maximumIndex],maximumIndex)
-        root = TreeNode(val=nums[maximumIndex],left= self.helper(nums,leftIndex,maximumIndex-1), right=self.helper(nums,maximumIndex+1,rightIndex) )
-        return root
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        candidates.sort()
+        print(candidates)
+        candDict = {}
+        currentCand = candidates[0]
+        currentRange = [0]
+        for x in range(len(candidates)):
+            if candidates[x] != currentCand:
+                currentRange.append(x-1)
+                candDict[currentCand] = currentRange
+                currentCand = candidates[x]
+                currentRange = [x]     
+        currentRange.append(len(candidates)-1)
+        candDict[candidates[-1]] = currentRange
+        print(candDict)
+        result = []
+        def helper(i,currentList,currentSum):
+            if currentSum == target:
+                print('adding',currentList)
+                result.append(currentList.copy())
+                return
+            if i >= len(candidates) or currentSum > target:
+                print('no no')
+                return
+            print('looking at index',i,':',candidates[i])
+            
+            # if currentSum + candidates[i] <= target:
+            start = candDict[candidates[i]][0]
+            print('start is',start)
+            end = candDict[candidates[i]][1]
+            print('end is',end)
+            copyList = currentList.copy()
+            for x in range(end-start+1):
+                copyList.append(candidates[i])
+                print('helper for',end+1,copyList,currentSum + (x+1)*candidates[i])
+                helper(end+1,copyList,currentSum + (x+1)*candidates[i])
+            # copyList.append(candidates[i])
+            # helper(i+1,copyList,currentSum + candidates[i])
+            
+            copyList = currentList.copy()
+            print('helper for',end+1,copyList,currentSum)
+            helper(end+1,copyList,currentSum)
+            
+        helper(0,[],0)
+        return result
 
-    def constructMaximumBinaryTree(self, nums: List[int]) -> Optional[TreeNode]:
-        root = self.helper(nums,0,len(nums)-1)
-        return root
-
-print()
-        
+s = Solution()
+print(s.combinationSum2([1,2,2,2,2,2,2,5],5))
--- DCP42.py
+++ DCP42.py
@@ -6,20 +6,57 @@
 
 # Integers can appear more than once in the list. You may assume all numbers in the list are positive.
 
-# For example, given S = [12, 1, 61, 5, 9, 2] and k = 24, return [12, 9, 2, 1] since it sums up to 24.
-
+# For example, given S = [12, 1, 61, 5, 9, 2] and k = 24, return [12, 9, 2, 1] since it sums up to 24.                     
+                                        
 from typing import List
+
 
 class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
-        memoryCells = [[None for _ in range(len(candidates) + 1)] for _ in range(target + 1)]
-        # for x in range(target + 1):
-        #     memoryCells[x][0] = False
-        for y in range(len(candidates) + 1):
-            memoryCells[0][y] = True
-        for x in range(1,target + 1):
-            for y in range(1,len(candidates) + 1):
-                memoryCells[x][y] = memoryCells[x-candidates[y-1]][y] or memoryCells[x][y-1]
-        print(memoryCells)
+        candidates.sort()
+        print(candidates)
+        candDict = {}
+        currentCand = candidates[0]
+        currentRange = [0]
+        for x in range(len(candidates)):
+            if candidates[x] != currentCand:
+                currentRange.append(x-1)
+                candDict[currentCand] = currentRange
+                currentCand = candidates[x]
+                currentRange = [x]     
+        currentRange.append(len(candidates)-1)
+        candDict[candidates[-1]] = currentRange
+        print(candDict)
+        result = []
+        def helper(i,currentList,currentSum):
+            if currentSum == target:
+                print('adding',currentList)
+                result.append(currentList.copy())
+                return
+            if i >= len(candidates) or currentSum > target:
+                print('no no')
+                return
+            print('looking at index',i,':',candidates[i])
+            
+            # if currentSum + candidates[i] <= target:
+            start = candDict[candidates[i]][0]
+            print('start is',start)
+            end = candDict[candidates[i]][1]
+            print('end is',end)
+            copyList = currentList.copy()
+            for x in range(end-start+1):
+                copyList.append(candidates[i])
+                print('helper for',end+1,copyList,currentSum + (x+1)*candidates[i])
+                helper(end+1,copyList,currentSum + (x+1)*candidates[i])
+            # copyList.append(candidates[i])
+            # helper(i+1,copyList,currentSum + candidates[i])
+            
+            copyList = currentList.copy()
+            print('helper for',end+1,copyList,currentSum)
+            helper(end+1,copyList,currentSum)
+            
+        helper(0,[],0)
+        return result
 
-        
+s = Solution()
+print(s.combinationSum2([1,2,2,2,2,2,2,5],5))
